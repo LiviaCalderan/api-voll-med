c886e7441352ed53989dc6f7fcbf10bfc742429a
Lívia Calderan <liv_calderan@hotmail.com>
2024-11-14 23:56:10 -0400

    Arrumando atributos de cancelamento da consulta

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -47,7 +47,7 @@ public class AgendaDeConsultas {
         if (medico == null) {
             throw new ValidacaoException("Não existe médico disponível nessa data!");
         }
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var consulta = new Consulta(null, medico, paciente, dados.data(),null);
         consultaRepository.save(consulta);
 
         return new DadosDetalhamentoConsulta(consulta);
